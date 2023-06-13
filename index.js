02b0a277ffca82a9ca0587dc2222337b4783cd95
njugudev <njugudev@pop-os.localdomain>
2023-06-13 09:04:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someVAlue){
   // Write your code here!
+  
+  if (someVAlue <= 400){
+      return 'This one is on me!'
+
+  } else if(someVAlue >= 2001 || someVAlue === 2500){
+      return 'I will gladly take your thirty bucks.'
+
+  } else if(someVAlue > 2500){
+      return 'No can do.'
+  }
+  
+  
+    
+ 
+  
+    
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code here! 
+  return(city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here!  
+  switch(tip){
+    case('generous'):
+      return "Thank you so much."
+      break;
+    case('not as generous'):
+      return "Thank you."
+      break;
+    case('thanks for everything'):
+      return 'Bye.'
+      break;
+    case('Bye'):
+      return 'Bye.'
+      break;
+    default:
+      tip = "Bye.";
+  }
+
 }
